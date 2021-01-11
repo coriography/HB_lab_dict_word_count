4f124858c00d3c90e380f58b284a778aac439615
Cori Lint <corilint@gmail.com>
2021-01-11 14:52:53 -0600

    Added code for count_words() function
    Co-authored by: Elizabeth Jamison <ejamison3@gmail.com>

--- wordcount.py
+++ wordcount.py
@@ -1,11 +1,27 @@
 # put your code here.
 
-# open file & assign to variable
-# initialize dictionary
-    # key = word, value = count of word
-# iterate through by line
-    # split line by space and put in list variable
-        # remove non-alpha characters (extra enhancement if time allows)
-    # loop through list
-        # add word to dictionary/update count (in dictionary)
-# unpack to print key/value pairs (using f string to print)
+
+# create a function with a parameter of filename
+def count_words(filename):
+    # open file & assign to variable
+    file = open(filename)
+    # initialize dictionary
+    words = {}
+        # key = word, value = count of word
+
+    num_lines = 0
+    # iterate through by line
+    for line in file:
+        # split line by space and put in list variable
+        line_words = line.lower().rstrip().split(' ')
+            # enhancement:
+                # remove non-alpha characters (extra enhancement if time allows)
+        # loop through list
+        for word in line_words:
+            # add word to dictionary/update count (in dictionary)
+            words[word] = words.get(word, 0) + 1
+
+    # unpack to print key/value pairs (using f string to print)
+    # enhancement idea - sort by word count
+    for unique_word, word_count in words.items():
+        print(f'{unique_word} {word_count}')
