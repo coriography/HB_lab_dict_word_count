21e107191f01ca4ca1a99783ec5bc6f796919dfc
Cori Lint <corilint@gmail.com>
2021-01-11 14:26:25 -0600

    Initial commit and pseudocode
    Co-authored by Elizabeth Jamison <ejamison3@gmail.com>

---
+++ wordcount.py
@@ -0,0 +1,11 @@
+# put your code here.
+
+# open file & assign to variable
+# initialize dictionary
+    # key = word, value = count of word
+# iterate through by line
+    # split line by space and put in list variable
+        # remove non-alpha characters (extra enhancement if time allows)
+    # loop through list
+        # add word to dictionary/update count (in dictionary)
+# unpack to print key/value pairs (using f string to print)
