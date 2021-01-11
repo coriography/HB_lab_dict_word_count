ad859070b1ebd4dd90242993af35c7d800ec699d
Cori Lint <corilint@gmail.com>
2021-01-11 15:20:47 -0600

    Made words lowercase and removed non-alpha characters.
    Co-authored by: Elizabeth Jamison <ejamison3@gmail.com>

--- wordcount.py
+++ wordcount.py
@@ -1,5 +1,5 @@
 # put your code here.
-
+import re
 
 # create a function with a parameter of filename
 def count_words(filename):
@@ -12,12 +12,12 @@ def count_words(filename):
     num_lines = 0
     # iterate through by line
     for line in file:
-        # split line by space and put in list variable
+        # split line by space, make all words lowercase, and put in list variable
         line_words = line.lower().rstrip().split(' ')
-            # enhancement:
-                # remove non-alpha characters (extra enhancement if time allows)
         # loop through list
         for word in line_words:
+            # remove non-alpha characters (extra enhancement if time allows)
+            word = re.sub(r'[^a-zA-Z]', '', word)
             # add word to dictionary/update count (in dictionary)
             words[word] = words.get(word, 0) + 1
 
